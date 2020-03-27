8833b3ba5f2682d755e72852511377cd32353b65
Akhilesh21 <akhilesh.mc4@gmail.com>
2020-03-27 14:06:35 +0530

    [Akhilesh] Refactor.added asyc function for date and reminder

--- Fundoo-Frontend-master/src/components/reminder.jsx
+++ Fundoo-Frontend-master/src/components/reminder.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
+
 class reminder extends Component {
     constructor(props){
         super(props)
         this.state={
             reminder: "",
         }
+    }
+    reminderMenuOpen = (e) =>{
+        this.setState({ reminderAnchorEl:e.currentTarget });
+    }
+    reminderMenuClose = () =>{
+        this.setState({reminderAnchorEl:null});
+    }
+    handleTodayDate = async () =>{
+        this.reminderMenuClose()
+        let date = new Date().toDateString();
+        let reminder = date + ", 8:00 PM";
     }
     render() {
         return (
