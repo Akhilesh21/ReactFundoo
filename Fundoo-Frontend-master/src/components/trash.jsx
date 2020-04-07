28db1f2fb78f66d09cfac595403b53e8a26952c6
Akhilesh21 <akhilesh.mc4@gmail.com>
2020-04-07 14:08:17 +0530

    [Akhilesh] Refactor.added imported  tooltip

--- Fundoo-Frontend-master/src/components/trash.jsx
+++ Fundoo-Frontend-master/src/components/trash.jsx
@@ -1,27 +1,75 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import { Tooltip, Card } from "@material-ui/core";
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import RestoreFromTrashIcon from '@material-ui/icons/RestoreFromTrash';
 class trash extends Component {
-    constructor(props){
-        super(props)
-        this.state={
-            note:[],
-            isDeleted:"",
-        }
-    }
-    render() {
-        return (
-            <div className="_notes">
-            <div className="_notes_" style={{marginTop:"95px",flexWrap:"wrap",}}>
-            return(
-                <div className="notes_">
-                
-                </div>
-            )
-            </div>
-            
-                
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      note: [],
+      isDeleted: "",
+    };
+  }
+  render() {
+    return (
+      <div className="_notes">
+        <div
+          className="_notes_"
+          style={{ marginTop: "95px", flexWrap: "wrap" }}
+        >
+          return(
+          <div className="notes_">
+            <Card
+              style={{ backgroundColor: this.props.color }}
+              className="get_Nottes_card"
+              style={{
+                width: "250px",
+                minHeight: "135px",
+                height: "auto",
+                margin: "5px",
+                padding: "10px",
+                boxShadow: "0px 1px 7px 0px",
+                marginTop: "10%",
+                borderRadius: "15px",
+                //background: key.data().color
+              }}
+            >
+
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  padding: "5px",
+                }}
+              >
+              <div>
+              <div>
+              {key.data().title}
+              </div>
+              <div style={{ marginTop: "25px" }}>
+              {key.data().decription}
+              </div>
+              </div>
+              </div>
+              <div className="getnoteicons_trash">
+              <div>
+              <Tooltip title="Delete forever">
+              <DeleteForeverIcon />
+              </Tooltip>
+              </div>
+              <div>
+              <Tooltip title="Restore">
+
+              </Tooltip>
+              </div>
+              </div>
+            </Card>
+          </div>
+          );
+        </div>
+      </div>
+    );
+  }
 }
 export default withRouter(trash);
