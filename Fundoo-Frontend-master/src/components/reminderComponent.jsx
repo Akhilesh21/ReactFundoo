b55a3b21e08b4f211641115e5149ddb72422ac90
Akhilesh21 <akhilesh.mc4@gmail.com>
2020-04-16 15:17:39 +0530

    [Akhilesh] Refactor.added mapping to reminder component

--- Fundoo-Frontend-master/src/components/reminderComponent.jsx
+++ Fundoo-Frontend-master/src/components/reminderComponent.jsx
@@ -1,39 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { getNotes } from "../Services/NoteServices";
 
 class reminderComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          notes: [],
-          istrash: "",
-        };
-        this.handleGetNotes();
-      }
-    
-      componentDidMount() {
-        this.handleGetNotes();
-      }
-      handleGetNotes = () => {
-        getNotes()
-          .then(async (res) => {
-            await this.setState({
-              notes: res.data.data,
-            });
-            console.log("res in notesData", this.state.notes);
-          })
-          .catch((err) => {
-            console.log("err", err);
-          });
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [],
+      istrash: "",
+    };
+    this.handleGetNotes();
+  }
 
-      
-    render() {
+  componentDidMount() {
+    this.handleGetNotes();
+  }
+  handleGetNotes = () => {
+    getNotes()
+      .then(async (res) => {
+        await this.setState({
+          notes: res,
+        });
+        console.log("res in notesData", this.state.notes);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+
+  render() {
+    let Obj = this.state.notes.map((key, index) => {
+      if ((key.reminder !== null) && (key.archieve === 0)) {
+        console.log("the dele is ", key.reminder);
         return (
-            <div>
-                
-            </div>
-        )
-    }
+          <div
+            style={{
+              height: "200px",
+              width: "400px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <span>{key.title}</span>
+            <span>{key.decription}</span>
+          </div>
+        );
+      }
+    });
+    return (
+      <div>
+        {Obj}
+      </div>
+    );
+  }
 }
+export default withRouter(reminderComponent);
