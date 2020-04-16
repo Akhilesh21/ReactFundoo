16bd2d590eaae046e806a07e69c786e7f9f9fe35
Akhilesh21 <akhilesh.mc4@gmail.com>
2020-04-16 16:10:35 +0530

    [Akhilesh] Refactor.added this , state to reminder component

--- Fundoo-Frontend-master/src/components/reminderComponent.jsx
+++ Fundoo-Frontend-master/src/components/reminderComponent.jsx
@@ -6,8 +6,17 @@ class reminderComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      notes: [],
-      istrash: "",
+        notes: [],
+        color: "",
+        noteId: "",
+        title: "",
+        description: "",
+        color: "",
+        istrash: 0,
+        archieve: 0,
+        ispinned: 0,
+        anchorEl: null,
+        reminder: null,
     };
     this.handleGetNotes();
   }
@@ -19,7 +28,7 @@ class reminderComponent extends Component {
     getNotes()
       .then(async (res) => {
         await this.setState({
-          notes: res,
+          notes: res.data.data,
         });
         console.log("res in notesData", this.state.notes);
       })
@@ -29,9 +38,9 @@ class reminderComponent extends Component {
   };
 
   render() {
-    let Obj = this.state.notes.map((key, index) => {
-      if ((key.reminder !== null) && (key.archieve === 0)) {
-        console.log("the dele is ", key.reminder);
+    let trashObj = this.state.notes.map((key, index) => {
+      if (key.istrash === 1) {
+        //console.log("the dele is ", key.reminder);
         return (
           <div
             style={{
@@ -49,7 +58,7 @@ class reminderComponent extends Component {
     });
     return (
       <div>
-        {Obj}
+        {trashObj}
       </div>
     );
   }
