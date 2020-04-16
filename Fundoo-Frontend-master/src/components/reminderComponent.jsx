640f4dff602884acdcb2df06b2812a5ef5d39657
Akhilesh21 <akhilesh.mc4@gmail.com>
2020-04-16 16:55:39 +0530

    [Akhilesh] Refactor.added ternary condition for pin and unpin to reminder  to reminder component

--- Fundoo-Frontend-master/src/components/reminderComponent.jsx
+++ Fundoo-Frontend-master/src/components/reminderComponent.jsx
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { getNotes } from "../Services/NoteServices";
-import {
-    Tooltip,
-    Card, Chip, Avatar
-} from "@material-ui/core";
+import { Tooltip, Card, Chip, Avatar } from "@material-ui/core";
 import unPin from "../assets/unpin.svg";
 import pin from "../assets/pin.svg";
 import AddAlertOutlinedIcon from "@material-ui/icons/AddAlertOutlined";
@@ -12,21 +9,24 @@ import ImageOutlinedIcon from "@material-ui/icons/ImageOutlined";
 import PersonAddOutlinedIcon from "@material-ui/icons/PersonAddOutlined";
 import MoreVertOutlinedIcon from "@material-ui/icons/MoreVertOutlined";
 import ColorComponent from "./colorNote";
+import Reminder from "./reminder";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import ArchiveOutlinedIcon from "@material-ui/icons/ArchiveOutlined";
 class reminderComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        notes: [],
-        color: "",
-        noteId: "",
-        title: "",
-        description: "",
-        color: "",
-        istrash: 0,
-        isarchive: 0,
-        ispinned: 0,
-        anchorEl: null,
-        reminder: null,
+      notes: [],
+      color: "",
+      noteId: "",
+      title: "",
+      description: "",
+      color: "",
+      istrash: 0,
+      isarchive: 0,
+      ispinned: Number,
+      anchorEl: null,
+      reminder: null,
     };
     this.handleGetNotes();
   }
@@ -48,116 +48,121 @@ class reminderComponent extends Component {
   };
 
   render() {
-      
     let trashObj = this.state.notes.map((key, index) => {
-      if ((key.reminder !== null) && (key.isarchive === 0)) {
+      if (key.reminder !== null && key.isarchive === 0) {
         //console.log("the dele is ", key.reminder);
         return (
-            <div className="notes_" >
+          <div className="notes_">
             <Card
-                className="get_Nottes_card"
-                style={{
-                    width: "250px",
-                    minHeight: "100px",
-                    height: "auto",
-                    margin: "5px",
-                    padding: "10px",
-                    boxShadow: "0px 1px 7px 0px",
-                    marginTop: "10%",
-                    borderRadius: "15px",
-                    background: key.color
-                }}
+              className="get_Nottes_card"
+              style={{
+                width: "250px",
+                minHeight: "100px",
+                height: "auto",
+                margin: "5px",
+                padding: "10px",
+                boxShadow: "0px 1px 7px 0px",
+                marginTop: "10%",
+                borderRadius: "15px",
+                background: key.color,
+              }}
             >
-            <div
-            style={{
-                display: "flex",
-                justifyContent: "space-between",
-                padding: "5px"
-            }}
-        >
-            <div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  padding: "5px",
+                }}
+              >
                 <div>
-                    {key.data().title}
+                  <div>{key.title}</div>
+                  <div>{key.decription}</div>
                 </div>
-                <div>{key.data().description}</div>
-
-            </div>
-            <div>
-                {key.data().reminder !== null ?
+                <div>
+                  {key.reminder !== null ? (
                     <Chip
-                        style={{ display: "flex", marginLeft: "-6em", marginTop: "5em" }}
-                        icon={<AccessTimeIcon />}
-                        label={key.data().reminder}
-                        onDelete={this.removeReminder}
-                        variant="outlined" />
-                    : null}
-            </div>
+                      style={{
+                        display: "flex",
+                        marginLeft: "-6em",
+                        marginTop: "5em",
+                      }}
+                      icon={<AccessTimeIcon />}
+                      label={key.reminder}
+                      onDelete={this.removeReminder}
+                      variant="outlined"
+                    />
+                  ) : null}
+                </div>
 
-            <div>
-                <Avatar style={{ background: "#d2cece", marginLeft: "-25px" }}
-                    onClick={() => this.handlePin(key.id)}>
-                    {key.data().isPinned === true ? < pin /> : <pin />}
-                </Avatar>
-
-            </div>
-        </div>
-        <div className="icons2">
-            <div>
-
-                <Reminder
+                <div>
+                  <div
+                    style={{ background: "#d2cece", marginLeft: "-25px" }}
+                    onClick={() => this.handlePin(key.id)}
+                  >
+                  {key.ispinned === 1 ? (
+                    <img className="pin-out" src={unPin} />
+                  ) : (
+                    <img className="pin-over" src={pin} />
+                  )}
+                  </div>
+                </div>
+              </div>
+              <div className="icons2">
+                <div>
+                  <Reminder
                     anchorEl={this.state.anchorEl}
                     closeMenu={this.handleClose}
                     handleGetNotes={this.handleGetNotes}
                     handleReminderDate={this.handleReminderDate}
-                />
-            </div>
-            <div>
-                <Tooltip title="Collbrate">
+                  />
+                </div>
+                <div>
+                  <Tooltip title="Collbrate">
                     <PersonAddOutlinedIcon />
-                </Tooltip>
-            </div>
-            <div>
-                <ColorComponent paletteProps={this.paletteProps} id={key.id} />
-            </div>
-            <div>
-                <Tooltip title="Add image">
+                  </Tooltip>
+                </div>
+                <div>
+                  <ColorComponent
+                    paletteProps={this.paletteProps}
+                    id={key.id}
+                  />
+                </div>
+                <div>
+                  <Tooltip title="Add image">
                     <ImageOutlinedIcon />
-                </Tooltip>
-            </div>
-            <div>
-                <Tooltip title="Un Archive">
+                  </Tooltip>
+                </div>
+                <div>
+                  <Tooltip title="Un Archive">
                     <div
-                        style={{ cursor: "pointer" }}
-                        onClick={() => this.archiveNote(key.id)}
+                      style={{ cursor: "pointer" }}
+                      onClick={() => this.archiveNote(key.id)}
                     >
-                        <ArchiveOutlinedIcon />
+                      <ArchiveOutlinedIcon />
                     </div>
-                </Tooltip>
-            </div>
-            <div>
-                <Tooltip title="More">
+                  </Tooltip>
+                </div>
+                <div>
+                  <Tooltip title="More">
                     <MoreVertOutlinedIcon
-                        onClick={this.menuItem}
-                        aria-owns="simple-menu" />
-                </Tooltip>
-                <More
+                      onClick={this.menuItem}
+                      aria-owns="simple-menu"
+                    />
+                  </Tooltip>
+                {/*  <More
                     anchorEl={this.state.anchorEl}
-                    closeMenu={this.handleClose} id={key.id}
+                    closeMenu={this.handleClose}
+                    id={key.id}
                     handleGetNotes={this.handleGetNotes}
-                />
-            </div>
-        </div>
-    </Card>
-</div>
-            
+                />*/}
+                </div>
+              </div>
+            </Card>
+          </div>
         );
       }
     });
-    return (
-      <div>
-        {trashObj}
-      </div>
-    );
+    return <div>{trashObj}</div>;
   }
 }
 export default withRouter(reminderComponent);
